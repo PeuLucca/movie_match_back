ea263d91ac23865764da25a5a5b092aa6b62378f
Pedro de Lucca <86271852+PeuLucca@users.noreply.github.com>
2024-02-26 17:22:07 -0300

    fixing comment

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controller/usersController');
 
-// get
-router.post('/returnUser', usersController.getUser);
-
 // post
 router.post('/validate', usersController.validateUser);
 router.post('/create', usersController.createUser);
+router.post('/returnUser', usersController.getUser);
 
 module.exports = router;
